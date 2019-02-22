31df1e7953b9d2a35518c0a215a0c1fffe55005f
Nicholas Eberts <nicholas.eberts@yahoo.com>
2019-02-22 09:45:56 -0500

    fixed hostname var

--- src/plan.sh
+++ src/plan.sh
@@ -62,7 +62,7 @@ CURRENT_PATH="$(dirname "$0")"
 . $CURRENT_PATH/init.sh -g $RESOURCE_GROUP_NAME -a $STORAGE_ACCOUNT_NAME -e $ENVIRONMENT
 
 # Pull down .tfvars to tmpfs from Azure Key Vault
-if [ $(hostname) == *_vs-agent_* ]; then az login -i; else echo "Using local token for az-cli"; fi
+if [[ $(hostname) == *"wk-caas"* ]]; then az login -i; else echo "Using local token for az-cli"; fi
 
 az keyvault secret show -n $TFVARS_SECRET --vault-name $KEYVAULT_NAME
 if [ $? -ne 0 ]
