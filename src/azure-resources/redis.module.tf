682d644c647e260337d6cffe155371d23e56e161
Thomas Gamble <thomas.gamble@microsoft.com>
2019-04-03 10:37:31 -0400

    working apply

--- src/azure-resources/redis.module.tf
+++ src/azure-resources/redis.module.tf
@@ -1,8 +1,8 @@
-module "redis" {
-  source = "./redis"
+# module "redis" {
+#   source = "./redis"
 
-  redis_name          = "${var.redis_name}"
-  resource_group_name = "${var.redis_resource_group_name}"
-  location            = "${var.location}"
-  redis_subnet_id     = "${module.vnet.vnet_subnets[1]}"
-}
+#   redis_name          = "${var.redis_name}"
+#   resource_group_name = "${var.redis_resource_group_name}"
+#   location            = "${var.location}"
+#   redis_subnet_id     = "${module.vnet.vnet_subnets[1]}"
+# }
